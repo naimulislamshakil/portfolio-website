d1009db5ae7a659204e1b824411dddbdb5864c64
naimulislamshakil <ms0413354@gmail.com>
2022-06-01 08:34:10 +0600

    use emailjs send user email to admin email

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Component/Pages/Home/Home";
 import Header from "./Component/Shered/Header/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
--- src/Component/Pages/Home/Contact.js
+++ src/Component/Pages/Home/Contact.js
@@ -1,6 +1,28 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { send } from "emailjs-com";
+import { toast } from "react-toastify";
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const formSubmit = (event) => {
+    event.preventDefault();
+
+    send(
+      "service_dhnbpha",
+      "template_m5aap6l",
+      { name, email, subject, message },
+      "Fkc4kg1aVX_E4HE2u"
+    ).then((res) => {
+      if (res.status === 200) {
+        toast.success("Email Send Successfull.");
+      }
+    });
+
+    event.target.reset();
+  };
+
   return (
     <div className="mt-5" id="contact">
       <div class="divider"></div>
@@ -11,10 +33,19 @@ const Contact = () => {
       <div>
         <div class="flex items-center justify-center py-5 bg-gray-200">
           <div class="w-full px-6 py-16 bg-white rounded-lg shadow-xl lg:w-2/5">
-            <form class="mx-8 space-y-4">
+            <form onSubmit={formSubmit} class="mx-8 space-y-4">
               <div>
                 <input
-                  type="text"
+                  type="name"
+                  onBlur={(e) => setName(e.target.value)}
+                  class="w-full p-2 text-sm border-b-2 border-gray-400 outline-none opacity-50 focus:border-blue-400"
+                  placeholder="Your Name"
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  onBlur={(e) => setEmail(e.target.value)}
                   class="w-full p-2 text-sm border-b-2 border-gray-400 outline-none opacity-50 focus:border-blue-400"
                   placeholder="Your Email"
                 />
@@ -22,6 +53,7 @@ const Contact = () => {
               <div>
                 <input
                   type="text"
+                  onBlur={(e) => setSubject(e.target.value)}
                   class="w-full p-2 text-sm border-b-2 border-gray-400 outline-none opacity-50 focus:border-blue-400"
                   placeholder="Subject"
                 />
@@ -29,14 +61,17 @@ const Contact = () => {
               <div>
                 <textarea
                   name="message"
+                  onBlur={(e) => setMessage(e.target.value)}
                   class="w-full p-6 text-sm border-b-2 border-gray-400 rounded-lg outline-none opacity-50 focus:border-blue-400"
                   placeholder="Enter your message"
                 ></textarea>
               </div>
 
-              <button class="block w-full px-2 py-4 mt-2 btn btn-outline btn-success rounded-full">
-                Send Massage
-              </button>
+              <input
+                type="submit"
+                value="Send Massage"
+                class="block w-full px-2 py-4 mt-2 btn btn-outline btn-success rounded-full"
+              ></input>
             </form>
           </div>
         </div>
